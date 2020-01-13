5a4a2356d9a4d285f67ca4efd57f7413842ba490
kimhyunyoung <>
2020-01-13 17:02:48 +0900

    component 테스트

--- src/components/Link/index.stories.tsx
+++ src/components/Link/index.stories.tsx
@@ -5,4 +5,5 @@ import Link from './index';
 
 storiesOf('Link', module)
   .add('Renders link to Google', () => <Link href="http://google.com">Link to Google</Link>)
+  .add('link test', () => <Link href="http://google.com">Link to Google</Link>)
   .add('Renders link to Google with classname', () => <Link href="http://google.com" className="my-link-class">Link to Google</Link>);
