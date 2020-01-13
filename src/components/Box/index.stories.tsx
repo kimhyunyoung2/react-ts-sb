c1549992a162140d3ac4fab6f993022d03f5d7a8
kimhyunyoung <>
2020-01-13 17:53:11 +0900

    test box component

--- src/components/Box/index.stories.tsx
+++ src/components/Box/index.stories.tsx
@@ -3,6 +3,6 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Box from './Box';
 
-storiesOf('Button', module)
-  .add('big button', () => <Box size={"big"}></Box>)
-  .add('small button', () => <Box size={"small"} ></Box>);
+storiesOf('Box', module)
+  .add('big Box', () => <Box size={"big"}></Box>)
+  .add('small Box', () => <Box size={"small"} ></Box>);
